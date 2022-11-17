e56fbb6cafd030dd7fc3e0ff19077446255ed25f
Frido Emans <verderster@gmail.com>
2022-11-17 14:43:06 +0100

    k6 load test for API

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ipfs-search-benchmark
 Collection of benchmarks for ipfs-search
+
+## filter log files:
+with an access.log file of the api, run the following to generate a paths file for the test:
+
+```
+grep "https://ipfs-search.com/" access.log | cut -d ' ' -f 7 > paths.txt
+```
---
+++ k6/loadTest.js
@@ -0,0 +1,62 @@
+/*
+K6 load test script for api.ipfs-search.com
+
+See https://k6.io/docs/get-started/installation/ for setup of k6
+
+To run, use:
+$ k6 run loadTest.js
+
+environnment variables:
+K6_VUS: # of virtual users, default 1
+K6_DURATION: script running duration, default '10s'
+PROTOCOL: http or https, default https
+HOST: host address of API, default 'api.ipfs-search.com'
+LOG_FILE: log file to take paths from. Default: 'paths.txt'
+
+ */
+
+
+// init context: importing modules
+import { SharedArray } from 'k6/data';
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+const PROTOCOL = __ENV.PROTOCOL || 'https'
+const API_URL = __ENV.HOST || 'api.ipfs-search.com'
+const LOG_FILE=__ENV.LOG_FILE || 'paths.txt'
+
+// init context: global variables
+const testPaths = SharedArray('test paths', () => {
+	const logFileContents = open(LOG_FILE);
+	return logFileContents.split(/\r?\n/)
+})
+
+// init context: define k6 options
+export const options = {
+	vus: 1,
+	duration: '10s',
+	thresholds: {
+		http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+		http_req_duration: ['avg < 150', 'p(90) < 200', 'p(95) < 500', 'p(99) < 1000'],
+	},
+};
+
+export function setup() {
+}
+
+export default function(data) {
+	const path = testPaths[Math.floor(Math.random() * testPaths.length)]
+	const res = http.get(`${PROTOCOL}://${API_URL}${path}`, {
+		tags: {
+			type: path.includes('metadata') ? 'metadata' : 'search',
+		}
+	});
+	check(res, {
+		'is status 200': (r) => r.status === 200,
+	});
+	sleep(1)
+}
+
+
+export function teardown(data) {
+}
